facd0cf043ba4e93e9a789a6fdadfe8f0de93a8f
HTFI physics <physics.htfi@gmail.com>
2024-12-31 22:52:30 +0900

    リファクタリング

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-// 特に必要ないので何もしない。
-// （キャッシュする場合はここでキャッシュに追加する。）
-self.addEventListener("install", () => undefined);
-
-// 必要なファイルはインターネット上から取得する。
-// （キャッシュを用意している場合は、キャッシュから取得する処理を書く。）
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
-});
