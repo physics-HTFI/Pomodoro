227e19f025080f014cdcb77f381b48caa5aa2a0c
HTFI physics <physics.htfi@gmail.com>
2025-01-12 13:28:28 +0900

    ボタン位置変更

--- js/style.js
+++ js/style.js
@@ -13,9 +13,9 @@ const buttonStyle =
 style.textContent =
   "#pomodoro-main {margin:0; background:#202124; display:flex; height:100svh; justify-content:center; align-items:center; user-select:none;}" +
   "#pomodoro-svg {width:100svmin; height:100svmin; position:fixed; font-size: 3%; font-family: fantasy;}" +
-  `#pomodoro-popup-button {bottom:0; right:0; ${buttonStyle}}` +
   `#pomodoro-counter {bottom:0; left:0; ${buttonStyle}}` +
-  `#pomodoro-speaker-button {top:0; right:0; ${buttonStyle}}` +
+  `#pomodoro-popup-button {top:0; right:0; ${buttonStyle}}` +
+  `#pomodoro-speaker-button {top:0; right:6svmin; ${buttonStyle}}` +
   `#pomodoro-speaker-select {z-index: 1;}` +
   "@media screen and (max-width: 600px) { #pomodoro-popup-button, #pomodoro-speaker-button {display: none;} }" +
   "";
