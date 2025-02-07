96e959d88caa13ed301a36f7261b86932b67f192
HTFI physics <physics.htfi@gmail.com>
2025-02-07 23:49:01 +0900

    修正：前回のデバイスが読み込まれないことがある

--- src/atoms/atomPlay/_atomDeviceId.ts
+++ src/atoms/atomPlay/_atomDeviceId.ts
@@ -3,4 +3,6 @@ import { atomWithStorage } from "jotai/utils";
 /**
  * 設定された値は`LocalStrage`に保存される
  */
-export const atomDeviceId = atomWithStorage("deviceID", "");
+export const atomDeviceId = atomWithStorage("deviceID", "", undefined, {
+  getOnInit: true,
+});
