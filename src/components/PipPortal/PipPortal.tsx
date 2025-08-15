c828ba1d05fea965b208adb987c2f59abf56cee7
HTFI physics <physics.htfi@gmail.com>
2025-08-15 22:33:29 +0900

    refactor: PipPortal

--- src/components/PipPortal/PipPortal.tsx
+++ src/components/PipPortal/PipPortal.tsx
@@ -1,19 +1,18 @@
 import { createPortal } from "react-dom";
-import { usePipPortal } from "./_usePipPortal";
-import { StyleCache } from "./_StyleCache";
+import { usePipPortal } from "./PipPortal.use";
+import { StyleCache } from "./ui/StyleCache";
+import { ReactNode } from "react";
 
 /**
  * ピクチャインピクチャ用のポータル（ReactコンポーネントをPiPに送った時に正常動作させるのに必要）
  */
-export function PipPortal({ children }: { children: React.ReactNode }) {
+export function PipPortal({ children }: { children: ReactNode }) {
   const { pipBody } = usePipPortal();
 
   children = <StyleCache>{children}</StyleCache>;
 
-  // pipBodyが値を持っている場合、ChildrenをpipBodyに移動する。
-  // pipBody === undefinedの場合、Childrenを本来の位置に表示する。
-  if (pipBody) {
-    return createPortal(children, pipBody);
-  }
-  return <>{children}</>;
+  // pipBodyが値を持っている場合、childrenをpipBodyに移動する。
+  // pipBody === undefinedの場合、childrenを本来の位置に表示する。
+  if (pipBody) return createPortal(children, pipBody);
+  return children;
 }
